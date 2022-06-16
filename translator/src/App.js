639300dab52fcafd12987ea6bf1e8d6d5a3b534f
SandunMadush <sandunmadushanka57@gmail.com>
2022-06-16 11:22:22 +0530

    edit code

--- translator/src/App.js
+++ translator/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Translate from './components/Translate';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <Translate/>
     </div>
   );
 }

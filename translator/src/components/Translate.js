a7fe5b0e1e382d1a21b223db6877b36a27c9bac2
SandunMadush <sandunmadushanka57@gmail.com>
2022-06-16 13:19:03 +0530

    add text area

--- translator/src/components/Translate.js
+++ translator/src/components/Translate.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import TextareaAutosize from "@mui/material/TextareaAutosize";
 
 export default function Translate() {
   return (
     <div>
       <div className="app-header">
-      <h2 className="header">Text Translator</h2>
+        <h2 className="header">Text Translator</h2>
+      </div>
+
+      <div className="app-body">
+        <div>
+          <form>
+            <TextareaAutosize
+              aria-label="empty textarea"
+              placeholder="Enter the text here..."
+              style={{ width: 400 }}
+            />
+          </form>
+        </div>
       </div>
     </div>
-  )
+  );
 }

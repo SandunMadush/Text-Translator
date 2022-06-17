4d62f246d901233d3514a60d1d60c22b31ae2174
SandunMadush <sandunmadushanka57@gmail.com>
2022-06-17 22:52:28 +0530

    use axios

--- translator/src/components/Translate.js
+++ translator/src/components/Translate.js
@@ -2,19 +2,48 @@ import React, { useState, useEffect } from "react";
 import TextareaAutosize from "@mui/material/TextareaAutosize";
 import Button from "@mui/material/Button";
 import TranslateIcon from "@mui/icons-material/Translate";
+import axios from "axios";
 
 export default function Translate() {
-  const [inputText, setInputText] = useState("");
-  const [resultTest, setResultText] = useState("");
-
-  const translateText = () => {
-    setResultText(inputText);
+  const [inputText, setInputText] = useState('');
+  const [resultTest, setResultText] = useState('');
+  const [detectLanguageKey, setdetectedLanguageKey] = useState('');
+  const [languagesList, setLanguagesList] = useState([]);
+  const [selectedLanguageKey, setLanguageKey] = useState('');
+  const getLanguageSource = () => {
+    axios.post('https://libretranslate.com/detect', {
+        q: inputText
+      })
+      .then((response) => {
+        setdetectedLanguageKey(response.data[0].language);
+      });
   };
+  useEffect(() => {
+    axios.get('shttps://libretranslate.com/languages').then((response) => {
+      setLanguagesList(response.data);
+    });
+    getLanguageSource()
+  }, [inputText]);
+  const languageKey = (selectedLanguage) => {
+    setLanguageKey(selectedLanguage.target.value)
+  }
+  const translateText = () => {
+    getLanguageSource();
 
+    let data = {
+        q : inputText,
+        source: detectLanguageKey,
+        target: selectedLanguageKey
+    }
+    axios.post("https://libretranslate.com/translate", data)
+    .then((response) => {
+        setResultText(response.data.translatedText)
+    })
+}
   return (
     <div>
       <div className="app-header">
-        <h2 className="header">Text Translator</h2>
+        <h2 className="header">Text-Translator</h2>
       </div>
 
       <div className="app-body">
@@ -33,13 +62,12 @@ export default function Translate() {
               class="form-select form-select-sm"
               className="language-select"
               aria-label=".form-select-sm example"
+              onChange={languageKey}
             >
-              <option selected>Select the Language</option>
-              <option value="1">English</option>
-              <option value="2">Spanish</option>
-              <option value="3">French</option>
-              <option value="4">Arabic</option>
-              <option value="5">Hindi</option>
+              <option>Please Select Language..</option>
+              {languagesList.map((language) => {
+                return <option value={language.code}>{language.name}</option>;
+              })}
             </select>
           </form>
           <form>
